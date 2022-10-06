7356ee35432af2cef010ebfa1c3a868017cf78c5
ayush-3516 <letsmail.ayushchaudhari@gmail.com>
2022-10-06 14:19:37 +0530

    Create Whitelist.sol

---
+++ contracts/Whitelist.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+contract Whitelist {
+    uint8 public maxWhitelistedAddresses = 50;
+    mapping(address => bool) public whitelistedAddresses;
+    uint8 public numAddressesWhitelisted;
+
+    function addAddressToWhitelist() public {
+        require(
+            !whitelistedAddresses[msg.sender],
+            "Sender has already been whitelisted"
+        );
+        require(
+            numAddressesWhitelisted < maxWhitelistedAddresses,
+            "More addresses cant be added, limit reached"
+        );
+        whitelistedAddresses[msg.sender] = true;
+        numAddressesWhitelisted += 1;
+    }
+}
